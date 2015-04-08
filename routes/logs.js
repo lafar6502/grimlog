a070786ef35e94bb67c32ea5d6f50aa5648fef6b
lafar6502 <rafal.gwizdala@gmail.com>
2015-04-08 23:51:49 +0200

    save

--- routes/logs.js
+++ routes/logs.js
@@ -22,13 +22,24 @@ router.get('/showlog/:id', function(req, res, next) {
     var lr = glog.openLogSearcher({
         fileName: logdir + '/' + req.params.id
     });
+    console.log('qry: ', req.query);
     
     var query = {
+        level: req.query.level,
+        logname: req.query.logname,
+        source: req.query.source,
+        correlation: req.query.correlation,
+        pid: req.query.pid,
+        threadid: req.query.threadid,
+        startTime: req.query.startTime,
+        endTime: req.query.endTime
     };
+    var st = isNaN(req.query.start) ? 0 : parseInt(req.query.start);
+    var lmt = isNaN(req.query.limit) ? 100 : parseInt(req.query.limit);
     
-    lr.search(query, 0, 100, 'entryid', 'asc', {}, function(s, r) {
+    lr.search(query, st, lmt, 'entryid', 'asc', {}, function(s, r) {
         lr.close();
-        console.log('result', s, r);
+        //console.log('result', s, r);
         res.render('showlog', {messages: r});
     });
     
