8e8b000d07931441e18d123c06f2c582c11c44b4
lafar6502 <rafal.gwizdala@gmail.com>
2015-04-18 13:03:13 +0200

    new ui for viewer

--- routes/logs.js
+++ routes/logs.js
@@ -21,6 +21,73 @@ router.get('/', function(req, res, next) {
     });
 });
 
+router.get('/query/:id', function(req, res, next) {
+    var fid = req.params.id;
+    if (fid.indexOf('.db3') < 0) fid += '.db3';
+    var lr = glog.openLogSearcher({
+        fileName: logdir + '/' + fid,
+        readOnly: true
+    });
+    
+    var stime = null;
+    if (!isNaN(req.query.hh) && !isNaN(req.query.mm) && !isNaN(req.query.ss) && !isNaN(req.query.baseDate)) {
+        //need a date, too!
+        stime = parseInt(req.query.baseDate) + (parseInt(req.query.hh) * 3600 + parseInt(req.query.mm) * 60 + parseInt(req.query.ss)) * 1000;
+    };
+    
+    console.log('stime: ', stime);
+    var query = _.omit({
+        level: req.query.level,
+        logname: req.query.logname,
+        source: req.query.source,
+        send_addr: req.query.send_addr,
+        correlation: req.query.correlation,
+        pid: req.query.pid,
+        threadid: req.query.threadid,
+        startTime: isNaN(req.query.startTime) ? stime : req.query.startTime,
+        endTime: req.query.endTime,
+        entryid: req.query.entryid
+    }, function(v) { return v == undefined || v == null || v == ''; });
+    
+    req.query.limit = isNaN(req.query.limit) ? 100 : parseInt(req.query.limit);
+    req.query.start = isNaN(req.query.start) ? 0 : parseInt(req.query.start);
+    lr.search(query, req.query.start, req.query.limit, 'entryid', 'asc', {}, function(s, r) {
+        lr.close();
+        //console.log('result', s, r);
+        if (!s) {
+            console.log('search query error', r);
+            res.status(500).end(r);
+            return;
+        };
+        
+        console.log('more? ', r.hasMore, r.start, r.limit);
+        r.form = req.query;
+        var t0 = r.data.length == 0 ? new Date() : new Date(r.data[0].ts);
+        if (!_.has(r.form, 'baseDate') || r.form.baseDate == null || r.form.baseDate == undefined || isNaN(r.form.baseDate)) {
+            r.form.baseDate = t0 - (t0 % (24 * 3600 * 1000));
+        };
+        if (isNaN(r.form.hh) || isNaN(r.form.mm) || isNaN(r.form.ss)) {
+            r.form.hh = t0.getHours();
+            r.form.mm = t0.getMinutes();
+            r.form.ss = t0.getSeconds();
+        }
+        _.forEach(r.data, function(e) {
+            e.tsf = moment(e.ts).format('HH:mm:ss.SSS');
+        });
+        res.render('query', r);
+    });
+    
+});
+
+router.get('/browser/:id', function(req, res, next) {
+    
+    var r = {
+        form: req.query
+    };
+    res.render('ajaxbrowser', r);
+
+});
+
 router.get('/showlog/:id', function(req, res, next) {
     var fid = req.params.id;
     if (fid.indexOf('.db3') < 0) fid += '.db3';
