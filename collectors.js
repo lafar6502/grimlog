f89db3dc1e0db1ec6389abfd3b02c3bdd9b9fad5
lafar6502 <rafal.gwizdala@gmail.com>
2015-04-21 20:40:39 +0200

    save

---
+++ collectors.js
@@ -0,0 +1,30 @@
+var glog = require('glog');
+
+
+
+function EventCollectors(cfg) {
+    console.log('configuring log collector', cfg);
+    var lc = glog.createLogCollector(cfg);
+    
+    this.start = function() {
+    };
+    
+    this.getLogCollector = function() {
+        return lc;
+    };
+    
+    this.close = function() {
+        lc.stop();
+    };
+};
+
+
+module.exports = {
+    configureEventCollectors: function(cfg) {
+        
+        var ec = new EventCollectors(cfg);
+        ec.start();
+        
+        return ec;
+    }
+}
